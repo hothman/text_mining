d753c7cfda291a2888a3d4a595629811bd273d17
Houcemeddine Othman <houcemoo@gmail.com>
2019-08-10 20:25:33 +0100

    Update parseMedline.py

--- src/parseMedline.py
+++ src/parseMedline.py
@@ -9,11 +9,7 @@ from nltk.probability import FreqDist
 from nltk.corpus import stopwords 
 from nltk.stem.snowball import SnowballStemmer
 from nltk import WordNetLemmatizer
-
-inputfile = "./onepaper.txt"
-myMEDLINE  = open(inputfile, 'r' )
-Medlinetxt = myMEDLINE.read()
-myMEDLINE.close()
+import argparse
 
 def parse(text):
 	hh = text.split(r'PMID-')
@@ -26,7 +22,11 @@ def parse(text):
 		mytext.append( text.replace('\n', ' ')  ) 
 	return ' '.join( mytext )
 
-	
+def get_PMID(text):
+	splitted=text.split()
+	print(splitted[1])
+
+
 def get_title(text):
 	mytext=text.split("\n")
 	for line in mytext:
@@ -34,7 +34,7 @@ def get_title(text):
 			title = line.split("TI  - ")[1]
 			return title 
 
-def tokenize_remove_stop_words(text): 
+def tokenize_remove_stop_words_stemmize(text): 
 	tokenized_text=word_tokenize(text.lower())
 	filtered_words = [word for word in tokenized_text if word not in stopwords.words('english')]
 	stemmer = SnowballStemmer("english")
@@ -48,4 +48,30 @@ def get_coutries_nationalities(country_list_file, file_nationalities):
 		nationalities = file.read().lower().splitlines() 
 	return countries, nationalities
 
+def check_occurence(countries, nationalities, corpus):
+	lexicon = countries+nationalities
+	for word in corpus: 
+		if word in lexicon:
+			print("PASS", word)
+			return "PASS"
 
+if __name__ == "__main__": 
+	parser = argparse.ArgumentParser(description="text mining for medline file format")
+	parser.add_argument("--medline", help="path to medline file")
+	parser.add_argument("--countries", help="path to the file containing the names of countries")
+	parser.add_argument("--nationalities", help="path to the file containing the nationalities")
+
+	args = parser.parse_args()
+
+	assert args.medline != None, 'You must provide a medline text file'
+	myMEDLINE  = open( args.medline, 'r' )
+	Medlinetxt = myMEDLINE.read()
+	myMEDLINE.close()
+	mytext =  parse(Medlinetxt)
+	mytitle = get_title(Medlinetxt)
+	get_PMID(Medlinetxt)
+	countries, nationalities = get_coutries_nationalities(args.countries, args.nationalities)
+	tokenized_text=tokenize_remove_stop_words_stemmize(mytext)
+	tokenized_title=tokenize_remove_stop_words_stemmize(mytitle)
+	text_title = tokenized_text + tokenized_title
+	check_occurence(countries, nationalities, text_title)
